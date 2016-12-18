215fb791ec43467f7d0edcee517de085bc33bf4b
Maciej Bendkowski <maciej.bendkowski@gmail.com>
2016-12-18 19:42:51 +0100

    Reorganize the Haskell MaybeT compiler

---
+++ src/Compiler/Haskell/Helpers.hs
@@ -0,0 +1,94 @@
+-- | Author: Maciej Bendkowski <maciej.bendkowski@tcs.uj.edu.pl>
+module Compiler.Haskell.Helpers where
+
+import Language.Haskell.Exts
+import Language.Haskell.Exts.SrcLoc (noLoc)
+
+import System
+import System.Boltzmann
+
+unname :: String -> QName
+unname = UnQual . Ident
+
+typeCons :: String -> Type
+typeCons = TyCon . unname
+
+typeVar :: String -> Type
+typeVar = TyVar . Ident
+
+varExp :: String -> Exp
+varExp = Var . unname
+
+conExp :: String -> Exp
+conExp = Con . unname
+
+toLit :: Integer -> Exp
+toLit = Lit . Int
+
+importType :: String -> ImportSpec
+importType = IThingAll . Ident 
+
+importFunc :: String -> ImportSpec
+importFunc = IVar . Ident 
+
+importFrom :: String -> [ImportSpec] -> ImportDecl
+importFrom mod specs = ImportDecl { importLoc = noLoc
+                                  , importModule = ModuleName mod
+                                  , importQualified = False
+                                  , importSrc = False
+                                  , importSafe = False
+                                  , importPkg = Nothing
+                                  , importAs = Nothing
+                                  , importSpecs = Just (False, specs)
+                                  }
+
+exportType :: String -> ExportSpec
+exportType = EThingAll . unname
+
+exportFunc :: String -> ExportSpec
+exportFunc = EVar . unname
+
+declTFun :: String -> Type -> [String] -> Exp -> [Decl]
+declTFun f type' args body = [decl, FunBind [main]]
+    where decl = TypeSig noLoc [Ident f] type'
+          args' = map (PVar . Ident) args
+          main = Match noLoc (Ident f) args' Nothing
+                       (UnGuardedRhs body) Nothing
+
+symbol :: String -> QOp
+symbol s = QVarOp $ UnQual (Symbol s)
+
+greater x = InfixApp x (symbol ">")
+less x = InfixApp x (symbol "<")
+lessEq x = InfixApp x (symbol "<=")
+lessF v x = less v (Lit $ Frac (toRational x))
+
+bind v = Generator noLoc (PVar $ Ident v)
+bindP x y = Generator noLoc (PTuple Boxed [PVar (Ident x), PVar (Ident y)])
+    
+sub x (Lit (Int 0)) = x
+sub x y = InfixApp x (symbol "-") y
+
+add x (Lit (Int 0)) = x
+add (Lit (Int 0)) x = x
+add x y = InfixApp x (symbol "+") y
+
+applyF :: Exp -> [Exp] -> Exp
+applyF = foldl App
+
+dot x = InfixApp x (symbol ".")
+
+declareADTs :: Real a => BoltzmannSystem b a -> [Decl]
+declareADTs sys = map declADT $ paramTypes sys
+
+declADT :: Real a => (String, [Cons a]) -> Decl
+declADT (t,cons) = DataDecl noLoc DataType [] (Ident t) []
+                            (map (QualConDecl noLoc [] [] . declCon) cons)
+                            [(unname "Show", [])]
+
+declCon :: Real a => Cons a -> ConDecl
+declCon expr = ConDecl (Ident $ func expr) ags
+    where ags = map declArg (args expr)
+
+declArg :: Arg -> Type
+declArg (Type s) = typeVar s
--- src/Compiler/Haskell/MaybeT.hs
+++ src/Compiler/Haskell/MaybeT.hs
@@ -1,7 +1,7 @@
 -- | Author: Maciej Bendkowski <maciej.bendkowski@tcs.uj.edu.pl>
 module Compiler.Haskell.MaybeT
     ( Configuration(..)
-    , compile 
+    , compile
     ) where
 
 import Language.Haskell.Exts
@@ -12,259 +12,7 @@ import System
 import System.Boltzmann
 
 import Compiler
-
-unname :: String -> QName
-unname = UnQual . Ident
-
-genName :: String -> String
-genName = (++) "genRandom"
-
-samplerName :: String -> String
-samplerName = (++) "sample"
-
-samplerIOName :: String -> String
-samplerIOName s = samplerName s ++ "IO"
-
-declTFun :: String -> Type -> [String] -> Exp -> [Decl]
-declTFun f type' args body = [decl, FunBind [main]]
-    where decl = TypeSig noLoc [Ident f] type'
-          args' = map (PVar . Ident) args
-          main = Match noLoc (Ident f) args' Nothing
-                       (UnGuardedRhs body) Nothing
-
-declPatternFun :: String -> [([Pat], Exp)] -> Decl
-declPatternFun f cases = FunBind body
-    where body = map match' cases
-          match' (pat,b) = Match noLoc (Ident f) pat Nothing
-                           (UnGuardedRhs b) Nothing
-
-maybeTType :: Type -> Type
-maybeTType = TyApp (TyApp (TyCon $ unname "MaybeT") 
-                            (TyApp (TyCon $ unname "Rand") (TyVar $ Ident "g")))
-
-randGenType :: String -> Type
-randGenType typeName = TyForall Nothing 
-    [ClassA (unname "RandomGen") [TyVar $ Ident "g"]]
-    (maybeTType $ TyCon (unname typeName))
-
-generatorType :: String -> Type
-generatorType typeName = TyForall Nothing 
-    [ClassA (unname "RandomGen") [TyVar $ Ident "g"]]
-    (TyFun (TyCon $ unname "Int")
-           (maybeTType $ TyTuple Boxed [TyCon $ unname typeName,
-                                        TyCon $ unname "Int"]))
-
-samplerType :: String -> Type
-samplerType typeName = TyForall Nothing 
-    [ClassA (unname "RandomGen") [TyVar $ Ident "g"]]
-    (TyFun (TyCon $ unname "Int") 
-           (TyFun (TyCon $ unname "Int") 
-                  (TyApp (TyApp (TyCon (unname "Rand")) (TyVar (Ident "g")))
-                  (TyCon (unname typeName)))))
-
-samplerIOType :: String -> Type
-samplerIOType typeName = TyForall Nothing []
-    (TyFun (TyCon $ unname "Int") 
-           (TyFun (TyCon $ unname "Int") 
-                  (TyApp (TyCon (unname "IO")) (TyVar (Ident typeName)))))
-
-compileModule sys moduleName withIO = Module noLoc (ModuleName moduleName) []
-                                        Nothing (Just exports) imports decls
-    where
-        exports = compileExports sys withIO
-        imports = compileImports withIO
-        decls = compileDecls sys withIO
-
-compileExports sys withIO = concatMap toADT $ typeList sys
-    where toADT' t = [EThingAll (unname t),
-                      EVar (unname $ genName t),
-                      EVar (unname $ samplerName t)]
-          toADT t
-            | not withIO = toADT' t
-            | otherwise = toADT' t ++ [EVar (unname $ samplerIOName t)]
-
-compileImports withIO = [import' "Control.Monad" ctrlMonadS,
-                         import' "Control.Monad.Random" ctrlRandS,
-                         import' "Control.Monad.Trans" ctrlTransS,
-                         import' "Control.Monad.Trans.Maybe" ctrlMaybeTS]
-    where
-        ctrlMonadS = Just (False, [IVar $ Ident "guard"])
-        ctrlTransS = Just (False, [IVar $ Ident "lift"])
-        ctrlMaybeTS = Just (False, [IThingAll $ Ident "MaybeT",
-                                    IVar $ Ident "runMaybeT"])
-        ctrlRandS
-            | not withIO = Just (False, [IThingAll $ Ident "RandomGen",
-                                         IVar $ Ident "Rand",
-                                         IVar $ Ident "getRandomR"])
-            
-            | otherwise = Just (False, [IThingAll $ Ident "RandomGen",
-                                        IVar $ Ident "Rand",
-                                        IVar $ Ident "getRandomR",
-                                        IVar $ Ident "evalRandIO"])
-
-import' name specs = ImportDecl { importLoc = noLoc
-                                , importModule = ModuleName name
-                                , importQualified = False
-                                , importSrc = False
-                                , importSafe = False
-                                , importPkg = Nothing
-                                , importAs = Nothing
-                                , importSpecs = specs
-                                }
-
-compileDecls sys withIO
-    | not withIO = dec
-    | otherwise = dec ++ declSamplersIO sys
-    where dec = declADTs sys ++ declRandGen 
-                    ++ declGenerators sys ++ declSamplers sys
-
-declSamplersIO :: Real a => BoltzmannSystem b a -> [Decl]
-declSamplersIO sys = concatMap declSamplerIO $ typeList sys
-
-declSamplerIO :: String -> [Decl]
-declSamplerIO t = declTFun (samplerIOName t) type' ["lb", "ub"] body
-    where type' = samplerIOType t
-          body = constructSamplerIO t
-
-declSamplers :: Real a => BoltzmannSystem b a -> [Decl]
-declSamplers sys = concatMap declSampler $ typeList sys
-
-declSampler :: String -> [Decl]
-declSampler t = declTFun (samplerName t) type' ["lb","ub"] body
-    where type' = samplerType t 
-          body = constructSampler t
-
-constructSampler :: String -> Exp
-constructSampler t = Do [runMaybeT', runSampler', case']
-    where
-        runMaybeT' = 
-            LetStmt $ BDecls [PatBind noLoc (PVar $ Ident "sampler")
-                             (UnGuardedRhs $ App (Var $ unname "runMaybeT")
-                                            (App (Var $ unname (genName t))
-                                            (Var $ unname "ub")))
-                                            Nothing]
-        runSampler' =
-            Generator noLoc (PVar $ Ident "x") (Var $ unname "sampler")
-
-        rec' = App (App (Var $ unname (samplerName t))
-                        (Var $ unname "lb")) (Var $ unname "ub")
-
-        case' = Qualifier $
-            Case (Var $ unname "x") [Alt noLoc (PApp (unname "Nothing") [])
-                                         (UnGuardedRhs rec') Nothing,
-                                    Alt noLoc (PApp (unname "Just") 
-                                        [PTuple Boxed [PVar $ Ident "t'",
-                                                       PVar $ Ident "s"]])
-                                        (UnGuardedRhs return') Nothing]
-        return' =
-            If (InfixApp (Var $ unname "lb")
-                         (QVarOp $ UnQual (Symbol "<="))
-                         (Var $ unname "s"))
-               (App (Var $ unname "return")
-                    (Var $ unname "t'"))
-               rec'
-
-constructSamplerIO :: String -> Exp
-constructSamplerIO t =  App (Var $ unname "evalRandIO")
-                            (App (App (Var $ unname (samplerName t)) 
-                                (Var $ unname "lb")) (Var $ unname "ub"))
-
-declRandGen :: [Decl]
-declRandGen = declTFun "randomP" type' [] body
-    where type' = randGenType "Double"
-          body = App (Var $ unname "lift") 
-                     (App (Var $ unname "getRandomR") 
-                          (Tuple Boxed [Lit $ Int 0, Lit $ Int 1]))
-
-declADTs :: Real a => BoltzmannSystem b a -> [Decl]
-declADTs sys = map declADT $ paramTypes sys
-
-declADT :: Real a => (String, [Cons a]) -> Decl
-declADT (t,cons) = DataDecl noLoc DataType [] (Ident t) []
-                            (map (QualConDecl noLoc [] [] . declCon) cons)
-                            [(unname "Show", [])]
-
-declCon :: Real a => Cons a -> ConDecl
-declCon expr = ConDecl (Ident $ func expr) ags
-    where ags = map declArg (args expr)
-
-declArg :: Arg -> Type
-declArg (Type s) = TyVar $ Ident s
-
-randomDraw :: String -> Stmt
-randomDraw var = Generator noLoc (PVar $ Ident var)
-                           (Var $ unname "randomP")
-
-declGenerators :: Real a => BoltzmannSystem b a -> [Decl]
-declGenerators sys = concatMap declGenerator $ paramTypes' sys
-
-declGenerator :: Real a => (String, [(Cons a, Integer)]) -> [Decl]
-declGenerator (t, g @ cons) = declTFun (genName t) type' ["ub"] body
-    where type' = generatorType t
-          body = constructGenerator g
-
-constructGenerator :: Real a => [(Cons a, Integer)] -> Exp
-constructGenerator [con] = returnCons "ub" con
-constructGenerator cons = Do [Qualifier $ guardian "ub",
-                              randomDraw "p", 
-                              Qualifier $ constructCons "p" "ub" cons]
-
-constructCons :: Real a => String -> String -> [(Cons a, Integer)] -> Exp
-constructCons p ub [c] = returnCons ub c
-constructCons p ub (c:cs) = 
-    constructIf p c (returnCons ub c) 
-                    (constructCons p ub cs)
-
-constructIf :: Real a => String -> (Cons a, t) -> Exp -> Exp -> Exp
-constructIf p (c,_) = If (InfixApp (Var $ unname p)
-                                   (QVarOp $ UnQual (Symbol "<"))
-                                   (Lit $ Frac (toRational $ weight c)))
-
-nat :: [String]
-nat = map show [0..]
-
-variableStream :: [String]
-variableStream = map ('x' :) nat
-
-weightVariableStream :: [String]
-weightVariableStream = map ('w' :) nat
-
-guardian :: String -> Exp
-guardian name = App (Var $ unname "guard") 
-                    (InfixApp (Var $ unname name)
-                              (QVarOp $ UnQual (Symbol ">"))
-                              (Lit $ Int 0))
-
-returnCons :: String -> (Cons a, Integer) -> Exp
-returnCons ub (con, w)
-    | null (args con) = App (Var $ unname "return")
-                            (Tuple Boxed [Con (unname $ func con), 
-                                          Lit (Int w)])
-    | otherwise = Do (buildArgs (args con) (InfixApp (Var $ unname ub)
-                                                     (QVarOp $ UnQual (Symbol "-"))
-                                                     (Lit $ Int w))
-                                           (Lit $ Int w)
-                                (Con (unname $ func con))
-                                variableStream weightVariableStream)
-
-buildArgs [] upperBound currW t vs ws = 
-    [Qualifier $ App (Var $ unname "return")
-                     (Tuple Boxed [t, currW])]
-
-buildArgs (Type a : as) upperBound currW t (v:vs) (w:ws) =
-    Generator noLoc (PTuple Boxed [pv', pw'])
-                    (App (Var $ unname (genName a)) upperBound)
-                    : buildArgs as upperBound' currW' t' vs ws
-    where
-        t' = App t v'
-        (v', w') = (Var $ unname v, Var $ unname w)
-        (pv', pw') = (PVar $ Ident v, PVar $ Ident w)
-        upperBound' = InfixApp upperBound (QVarOp $ UnQual (Symbol "-")) w'
-        currW' = InfixApp currW (QVarOp $ UnQual (Symbol "+")) w'
-
-moduleHeader :: Show a => BoltzmannSystem b a -> String -> String
-moduleHeader sys compilerNote = unlines ["-- | Compiler: " ++ compilerNote,
-                                         "-- | Singularity: " ++ show (parameter sys)]
+import Compiler.Haskell.Helpers
 
 data Configuration b a = Configuration { paramSys :: BoltzmannSystem b a
                                        , moduleName :: String
@@ -281,3 +29,172 @@ instance (Real a, Show a) => Compilable (Configuration b a) where
                    in do
                        putStr $ moduleHeader sys note
                        putStrLn $ prettyPrint module'
+
+moduleHeader :: Show a => BoltzmannSystem b a -> String -> String
+moduleHeader sys compilerNote = unlines ["-- | Compiler: " ++ compilerNote,
+                                         "-- | Singularity: " ++ show (parameter sys)]
+
+compileModule sys mod withIO = Module noLoc (ModuleName mod) []
+                                      Nothing (Just exports) imports decls
+    where
+        exports = declareExports sys withIO
+        imports = declareImports withIO
+        decls = declareADTs sys ++
+                    declareGenerators sys ++
+                    declareSamplers sys ++
+                    declareSamplersIO sys withIO
+
+declareImports withIO = 
+    [importFrom "Control.Monad" [importFunc "guard"],
+     importFrom "Control.Monad.Trans" [importFunc "lift"],
+     importFrom "Control.Monad.Trans.Maybe" [importType "MaybeT",
+                                             importFunc "runMaybeT"],
+     
+     importFrom "Control.Monad.Random" ([importType "RandomGen",
+                                        importFunc "Rand",
+                                        importFunc "getRandomR"]
+                                        ++ importIO withIO)]
+
+importIO False = []
+importIO True = [importFunc "evalRandIO"]
+
+-- Naming functions.
+genName = (++) "genRandom"
+samplerName = (++) "sample"
+samplerIOName t = samplerName t ++ "IO"
+
+declareExports sys withIO =
+    exportGenerators sys ++
+    exportSamplers sys ++
+    exportSamplersIO sys withIO
+
+exportGenerators sys = map (exportFunc . genName) $ typeList sys
+exportSamplers sys = map (exportFunc . samplerName) $ typeList sys
+
+exportSamplersIO sys False = []
+exportSamplersIO sys True = map (exportFunc . samplerIOName) $ typeList sys
+
+-- Utils.
+maybeT' = typeCons "MaybeT"
+rand' = typeCons "Rand"
+int' = typeCons "Int"
+g' = typeVar "g"
+
+randomGen' = unname "RandomGen"
+return' = varExp "return"
+
+nat :: [String]
+nat = map show [0..]
+
+variableStream :: [String]
+variableStream = map ('x' :) nat
+
+weightStream :: [String]
+weightStream = map ('w' :) nat
+
+-- Generators.
+maybeTType :: Type -> Type
+maybeTType = TyApp (TyApp maybeT' (TyApp rand' g'))
+
+generatorType :: Type -> Type
+generatorType type' = TyForall Nothing 
+    [ClassA randomGen' [g']] 
+    (TyFun int' (maybeTType $ TyTuple Boxed [type', int']))
+
+declRandomP :: [Decl]
+declRandomP = declTFun "randomP" type' [] body
+    where type' = TyForall Nothing [ClassA randomGen' [g']] (maybeTType $ typeVar "Double")
+          body = App (varExp "lift") 
+                     (App (varExp "getRandomR") 
+                          (Tuple Boxed [toLit 0, toLit 1]))
+
+randomP v = bind v $ varExp "randomP"
+guardian v = Qualifier $ App (varExp "guard") 
+                             (varExp v `greater` toLit 0) 
+
+declareGenerators sys = 
+    declRandomP ++ 
+        concatMap declGenerator (paramTypes' sys)
+
+declGenerator (t, g @ cons) = declTFun (genName t) type' ["ub"] body
+    where type' = generatorType $ typeCons t
+          body = constrGenerator g 
+
+constrGenerator [(constr, w)] = rec constr w
+constrGenerator cs = Do (initSteps ++ branching)
+    where branching = [Qualifier $ constrGenerator' cs]
+          initSteps = [guardian "ub", 
+                       randomP "p"]
+                    
+constrGenerator' [(constr, w)] = rec constr w
+constrGenerator' ((constr, w) : cs) =
+    If (lessF (varExp "p") $ weight constr)
+       (rec constr w)
+       (constrGenerator' cs)
+
+rec constr w = case arguments (args constr) (toLit w) variableStream weightStream of
+                 ([], _, _) -> applyF return' [Tuple Boxed [cons, toLit w]]
+                 (stmts, totalW, xs) -> Do (stmts ++ [ret cons xs (toLit w `add` totalW)])
+    
+    where cons = conExp $ func constr
+
+arguments [] ub _ _ = ([], toLit 0, [])
+arguments (Type arg:args) ub (x:xs) (w:ws) = (stmt : stmts, argW', v : vs)
+    where stmt = bindP x w $ applyF (varExp $ genName arg) [varExp "ub" `sub` ub]
+          (stmts, argW, vs) = arguments args ub' xs ws
+          argW' = argW `add` varExp w
+          ub' = ub `sub` varExp w
+          v = varExp x
+
+ret f [] w = Qualifier $ applyF return' [Tuple Boxed [f, w]]
+ret f xs w = Qualifier $ applyF return' [Tuple Boxed [t, w]]
+    where t = applyF f xs
+
+-- Samplers.
+samplerType :: Type -> Type
+samplerType type' = TyForall Nothing 
+    [ClassA randomGen' [g']]
+    (TyFun int' 
+           (TyFun int'
+                  (TyApp (TyApp rand' g') type')))
+
+declareSamplers sys = concatMap declSampler $ typeList sys
+
+declSampler :: String -> [Decl]
+declSampler t = declTFun (samplerName t) type' ["lb","ub"] body
+    where type' = samplerType (typeCons t) 
+          body = constructSampler t
+
+constructSampler t =
+    Do [bind "sample" (applyF (varExp "runMaybeT") 
+            [applyF (varExp $ genName t) [varExp "ub"]]),
+            caseSample t] 
+    where caseSample t = Qualifier $ Case (varExp "sample") 
+                 [Alt noLoc (PApp (unname "Nothing") [])
+                        (UnGuardedRhs rec') Nothing,
+                        Alt noLoc (PApp (unname "Just") 
+                 [PTuple Boxed [PVar $ Ident "x",
+                  PVar $ Ident "s"]])
+                  (UnGuardedRhs return') Nothing]
+
+          rec' = applyF (varExp $ samplerName t) [varExp "lb", varExp "ub"]
+          return' = If (lessEq (varExp "lb") (varExp "s"))
+                      (applyF (varExp "return") [varExp "x"])
+                      rec'
+
+-- IO Samplers.
+samplerIOType :: Type -> Type
+samplerIOType type' = TyForall Nothing 
+    [] (TyFun int' (TyFun int' (TyApp (typeVar "IO") type')))
+
+declareSamplersIO sys False = []
+declareSamplersIO sys True = concatMap declSamplerIO $ typeList sys
+
+declSamplerIO :: String -> [Decl]
+declSamplerIO t = declTFun (samplerIOName t) type' ["lb","ub"] body
+    where type' = samplerIOType (typeCons t) 
+          body = constructSamplerIO t
+
+constructSamplerIO t = applyF (varExp "evalRandIO")
+                              [applyF (varExp $ samplerName t) [varExp "lb",
+                                                                varExp "ub"]]
